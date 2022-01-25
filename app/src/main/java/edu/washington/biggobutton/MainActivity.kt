5cc0947d4fc9e9de0cdc0980b02c095a37bedf4b
LukeDors <ldorsett@uw.edu>
2022-01-24 21:07:02 -0600

    app passes checks

--- app/src/main/java/edu/washington/biggobutton/MainActivity.kt
+++ app/src/main/java/edu/washington/biggobutton/MainActivity.kt
@@ -2,10 +2,19 @@ package edu.washington.biggobutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bigButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var count = 0
+        bigButton = findViewById<Button>(R.id.bigButton)
+        bigButton.setOnClickListener {
+            count += 1
+            val presses = if (count === 1) "time" else "times"
+            bigButton.setText(getString(R.string.buttonPress, count, presses))
+        }
     }
 }
